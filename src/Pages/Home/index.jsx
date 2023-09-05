79c1ed908bd866003caa7e43bec7ec4a25dc0064
CodeSandbox <support@codesandbox.io>
2023-09-05 02:41:15 +0000

    Initial commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import Layout from '../../Components/Layout'
+import Card from '../../Components/Card'
+import ProductDetail from '../../Components/ProductDetail'
+
+function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+  return (
+    <Layout>
+      Home
+      <div className='grid gap-2 grid-cols-3 w-full max-w-screen-lg justify-center'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
+      </div>
+      <ProductDetail />
+    </Layout>
+  )
+}
+
+export default Home
